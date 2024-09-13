1e858da61f564631b2b5dced58872f4c6afee94f
Bruno <bruno.pereira.pinho@gmail.com>
2024-09-13 19:47:19 -0300

    Initial commit

---
+++ src/main/java/tech/pinhos/financas/controller/HelloWorld.java
@@ -0,0 +1,15 @@
+package tech.pinhos.financas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorld {
+
+    @RequestMapping("hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello World";
+    }
+}
